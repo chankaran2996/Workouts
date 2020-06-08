65cef38da76bf9aaec5c0edde82c0850bb3fb653
chankaran2996 <45528864+chankaran2996@users.noreply.github.com>
2020-06-08 19:28:25 +0530

    Update chunkTest.js

--- mochainit/test/chunkTest.js
+++ mochainit/test/chunkTest.js
@@ -2,7 +2,7 @@ const expect = require('expect.js');
 const chunk = require("./code/chunk.js")
 describe('User', function() {
     it('Array split element excess reming after splited into given number', function() {
-        return expect(chunk([1,3,5,2,8,90,91,63,44],4)).to.be([1,3,5,2],[8,90,91,63],[4])
+        return expect(chunk([1,3,5,2,8,90,91,63,44],4)).to.be([1,3,5,2],[8,90,91,63],[44])
     })
     it('Array element not splited into given number', function() {
         return expect(chunk([0,-1,67,33,-55,-40,-7,3,6],3)).to.be([0,-1,67],[33,-55,-40],[-7,3,6])
