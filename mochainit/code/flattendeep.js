3a531e01b1c7738e1d7f4471ee8c988be2d00e7c
chankaran2996 <45528864+chankaran2996@users.noreply.github.com>
2020-06-09 17:36:56 +0530

    Add files via upload

---
+++ mochainit/code/flattendeep.js
@@ -0,0 +1,18 @@
+// Recursively flattens array.
+
+// Arguments :
+// array (Array): The array to flatten.
+
+// Returns :
+// (Array): Returns the new flattened array.
+
+// Example
+// flattenDeep([1, [2, [3, [4]],  5]]);
+// // => [1, 2, 3, 4, 5]
+
+var flattenDeep;
+flattenDeep = function(arr){
+//code here
+};
+
+module.exports =flattenDeep;
