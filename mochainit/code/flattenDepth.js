a8bfa7ea21145f1d08c30103089ec2bed0b9de80
chankaran2996 <45528864+chankaran2996@users.noreply.github.com>
2020-06-09 17:53:27 +0530

    Add files via upload

---
+++ mochainit/code/flattenDepth.js
@@ -0,0 +1,25 @@
+// Recursively flatten array up to depth times.
+
+// Arguments :
+// array (Array): The array to flatten.
+
+// [depth=1] (number): The maximum recursion depth.
+
+// Returns :
+// (Array): Returns the new flattened array.
+
+// Example
+// var array = [1, [2, [3, [4]],  5]];
+
+// flattenDepth(array,  1);
+// // => [1, 2, [3, [4]], 5]
+
+// flattenDepth(array,  2);
+// // => [1, 2, 3, [4], 5]
+
+var flattenDepth;
+flattenDepth = function(value,index,arr){
+//code here
+};
+
+module.exports =flattenDepth;
---
+++ mochainit/code/frompairs.js
@@ -0,0 +1,18 @@
+// this method returns an object composed from key-value pairs.
+
+// Arguments :
+// pairs (Array): The key-value pairs.
+
+// Returns :
+// (Object): Returns the new object.
+
+// Example
+// fromPairs([['a', 1], ['b', 2]]);
+// // => { 'a': 1, 'b': 2 }
+
+var fromPairs;
+fromPairs = function(arr){
+//code here
+};
+
+module.exports =fromPairs;
